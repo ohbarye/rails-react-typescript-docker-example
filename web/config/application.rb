8d47559c8fa9e13382dac7487b932ad94189eadf
ohbarye <over.rye@gmail.com>
2018-07-07 21:07:28 +0900

    Add rack-cors to allow cross origin request for development

--- web/config/application.rb
+++ web/config/application.rb
@@ -15,5 +15,12 @@ module Myapp
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: [:get, :post, :options]
+      end
+    end
   end
 end
