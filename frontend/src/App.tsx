10aa41ff69bf95d4b1b3906abcbc15f143a54b63
ohbarye <over.rye@gmail.com>
2019-08-18 20:35:52 +0900

    yarn eject && connect to backend server

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,32 @@
 import React from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
+const fetchContent = async (updateContent: (content: string) => void) => {
+  const response = await axios.get('http://localhost:3001/greetings/hello',{
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+    },
+  });
+  updateContent(response.data.content)
+};
+
 const App: React.FC = () => {
+  const [content, updateContent] = React.useState('Waiting for a response from Rails...');
+
+  React.useEffect(() => {
+    fetchContent(updateContent);
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          {content}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
